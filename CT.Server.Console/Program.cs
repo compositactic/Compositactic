0e5cf95090420be93472b51b48267c2a88be2bac
Matt J. Crouch <38637003+compositactic@users.noreply.github.com>
2019-01-16 22:23:19 -0500

    Add files via upload

---
+++ CT.Server.Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using CT.Hosting.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configurationFile = Path.Combine(Environment.CurrentDirectory, "ShopMonitorConfig.json");
+            var shopMonitorConfiguration = JsonConvert.DeserializeObject<RootHttpServerConfiguration>(File.ReadAllText(configurationFile));
+            
+            using (var server = CompositeRootHttpServerConfiguration.Create(shopMonitorConfiguration).CreateServer())
+            {
+                server.Start();
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
+
+
+//http://stackoverflow.com/questions/11403333/httplistener-with-https-support
+//http://stackoverflow.com/questions/499591/are-https-urls-encrypted
