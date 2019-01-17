0b3a374b200d97f560df8b8991f61f6f46c3aaa8
Matt J. Crouch <38637003+compositactic@users.noreply.github.com>
2019-01-16 22:23:44 -0500

    Add files via upload

---
+++ CT.Server.Foo.Console/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.Serialization;
+using CT.Hosting;
+using CT.Hosting.Configuration;
+using CT;
+
+namespace FooServer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var serverUrl = "http://localhost:8002/Chat/";
+            using (var server = CompositeRootHttpServerConfiguration.Create(serverUrl,
+                                typeof(ChatRoot),
+                                CompositeRootMode.SingleHost)
+                                .CreateServer())
+            {
+                server.Start();
+                Console.WriteLine("Navigate to: " + serverUrl);
+                Console.ReadLine();
+            }
+        }
+    }
+
+    [DataContract]
+    public class ChatRoot : CompositeRoot
+    {
+        public ChatRoot(CompositeRootConfiguration configuration) : base(configuration) { }
+
+        [Command]
+        public void SaySomething(CompositeRootHttpContext context, string message)
+        {
+            AddEvent(CompositeEventType.Custom, string.Empty, context.Request.UserName + " says: " + message);
+        }
+    }
+}
