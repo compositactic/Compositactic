757b63d35ecdb420bc97142881298a04fcb8341d
Matt J. Crouch <matt_crouch@hotmail.com>
2019-03-15 22:28:41 -0400

    Began work on Test application SQL scripts

---
+++ CT.Test.Presentation/000ShopServerDatabase.sql
@@ -0,0 +1,2 @@
+﻿IF (db_id(N'{0}') IS NULL)
+	CREATE DATABASE "{0}"
---
+++ CT.Test.Presentation/001Util.sql
@@ -0,0 +1,59 @@
+﻿
+CREATE OR ALTER PROCEDURE dbo.CreateTable 
+    @tableName NVARCHAR(MAX),
+	@parentTableName NVARCHAR(MAX) = ''
+AS
+	DECLARE @sql NVARCHAR(MAX)
+
+	SET @sql = 'IF NOT EXISTS (SELECT * FROM sys.tables WHERE Name = ''' + @tableName + ''')
+		CREATE TABLE ' + @tableName + ' ( [Id] INT NOT NULL PRIMARY KEY IDENTITY '
+	IF @parentTableName = ''
+		SET @sql = @sql + ')'
+	ELSE
+		SET @sql = @sql + ', [' + @parentTableName + 'Id] INT NOT NULL,
+			CONSTRAINT [FK_' + @tableName + '_To_' + @parentTableName + '] FOREIGN KEY ([' + @parentTableName + 'Id]) REFERENCES ['  + @parentTableName + ']([Id]))'
+	PRINT @sql
+	EXEC sp_executesql @sql
+
+GO
+
+CREATE OR ALTER PROCEDURE dbo.DropTable 
+    @tableName NVARCHAR(MAX) 
+AS
+	DECLARE @sql NVARCHAR(MAX)
+
+	SET @sql = 'IF EXISTS (SELECT * FROM sys.tables WHERE Name = ''' + @tableName + ''')
+		DROP TABLE ' + @tableName 
+	
+	EXEC sp_executesql @sql
+
+GO
+
+CREATE OR ALTER PROCEDURE dbo.CreateOrModifyColumn
+	@tableName NVARCHAR(MAX),
+	@columnName NVARCHAR(MAX),
+	@columnType NVARCHAR(MAX)
+AS
+	DECLARE @sql NVARCHAR(MAX)
+
+	SET @sql = 'IF COL_LENGTH(''' + @tableName + ''',''' + @columnName + ''') IS NULL
+					ALTER TABLE ' + @tableName + ' ADD ' + @columnName + ' ' + @columnType + 
+				'ELSE 
+					ALTER TABLE ' + @tableName + ' ALTER COLUMN ' + @columnName + ' ' + @columnType 
+	EXEC sp_executesql @sql
+
+GO
+
+CREATE OR ALTER PROCEDURE dbo.DropColumn
+	@tableName NVARCHAR(MAX),
+	@columnName NVARCHAR(MAX)
+AS
+	DECLARE @sql NVARCHAR(MAX)
+
+	SET @sql = 'IF COL_LENGTH(''' + @tableName + ''',''' + @columnName + ''') IS NOT NULL
+					ALTER TABLE ' + @tableName + ' DROP COLUMN ' + @columnName  
+	EXEC sp_executesql @sql
+
+
+GO
+
