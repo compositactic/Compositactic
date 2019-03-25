8d80b95877e541725c1c1be2a91f84f5cdefb640
Matt J. Crouch <matt_crouch@hotmail.com>
2019-03-24 22:35:52 -0400

    Beginnings of functional design documentation

---
+++ CT.Test/System/Stories/My Sample Story/Use Cases/Create A Post/CreateAPost.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CT.Blog.Test.System.Stories.My_Sample_Story.Use_Cases.Create_A_Post
+{
+    [TestClass]
+    public class CreateAPost
+    {
+        [ClassInitialize]
+        public static void Setup(TestContext testContext)
+        {
+        }
+
+        [ClassCleanup]
+        public static void Shutdown()
+        {
+
+        }
+
+        [TestMethod]
+        public void MyTest()
+        {
+        }
+
+    }
+}
